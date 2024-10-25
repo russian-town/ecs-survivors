9c15628daba788548528a987a313e9a0ba0522a7
Yuriy <russian_town_98@mail.ru>
2024-10-25 20:16:27 +0600

    Separated_Contexts

---
+++ src/ecs-survivors/Assets/Code/Common/Destruct/Systems/CleanupMetaDestructedSystem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Entitas;
+
+namespace Code.Common.Destruct.Systems
+{
+    public class CleanupMetaDestructedSystem : ICleanupSystem
+    {
+        private readonly IGroup<MetaEntity> _entities;
+        private readonly List<MetaEntity> _buffer = new (16);
+
+        public CleanupMetaDestructedSystem(MetaContext meta) => 
+            _entities = meta.GetGroup(MetaMatcher.Destructed);
+
+        public void Cleanup()
+        {
+            foreach (var entity in _entities.GetEntities(_buffer)) 
+                entity.Destroy();
+        }
+    }
+}
